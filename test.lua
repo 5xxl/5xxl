6bcf390ce12060827426106084e9cb02ee13372b
ادريس <91016285+5xxl@users.noreply.github.com>
2022-12-13 03:58:10 -0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,14 +1,13 @@
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/GemstoneDev/Wally-V3-Backup/main/script/main.lua')))()
 
-local w = library:CreateWindow("A") 
+local win = library:CreateWindow("A") 
 
-local b = w:CreateFolder("Walk") 
+local b = win:CreateFolder("Walk") 
 local plr = game:GetService("Players").LocalPlayer
-
-b:slider("Hcker ",{
-    min = 16,
-    max = 100,
-    precize = false 
+b:Slider("Slider",{
+    min = 10; 
+    max = 50; 
+    precise = true;
 },function(v)
     plr.Character.Humanoid.WalkSpeed = v
 end)
